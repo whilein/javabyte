0be0e4a091ab908d2f0c6558a5844e44f301e23b
Whilein <whilein.1@gmail.com>
2021-07-30 19:28:17 +0300

    build(test): new compare, equals, hashCode & toString tests

---
+++ src/test/java/javabyte/compare/CompareTests.java
@@ -0,0 +1,154 @@
+/*
+ *    Copyright 2021 Whilein
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package javabyte.compare;
+
+import javabyte.EqualityStrategy;
+import javabyte.Javabyte;
+import javabyte.TestClassLoader;
+import javabyte.bytecode.Asm;
+import javabyte.opcode.JumpOpcode;
+import lombok.SneakyThrows;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author whilein
+ */
+final class CompareTests {
+    String testName;
+
+    @BeforeEach
+    void setup(final TestInfo testInfo) {
+        testName = testInfo.getDisplayName();
+    }
+
+    @Test
+    @DisplayName("CompareArrays")
+    void compareArrays() {
+        val compare = generate(CompareArrays.class, EqualityStrategy.CONTENTS);
+        assertTrue(compare.compare(new String[] { "1", "2", "3" }, new String[] { "1", "2", "3" }));
+        assertFalse(compare.compare(new String[] { "1", "2", "3" }, new String[] { "3", "2", "1" }));
+    }
+
+    @Test
+    @DisplayName("CompareObjects_Safe")
+    void compareObjects_Safe() {
+        val compare = generate(CompareObjects.class, EqualityStrategy.SAFE);
+
+        assertTrue(compare.compare(1, 1));
+        assertFalse(compare.compare(1, null));
+        assertFalse(compare.compare(null, 1));
+    }
+
+    @Test
+    @DisplayName("CompareObjects_Ref")
+    void compareObjects_Ref() {
+        val compare = generate(CompareObjects.class, EqualityStrategy.REF);
+
+        Integer a = 123123;
+        Integer b = 123123;
+
+        assertTrue(compare.compare(a, a));
+        assertFalse(compare.compare(a, b));
+    }
+
+    @Test
+    @DisplayName("compareObjects_Default")
+    void compareObjects_Default() {
+        val compare = generate(CompareObjects.class, EqualityStrategy.DEFAULT);
+        assertTrue(compare.compare("ABC", "ABC"));
+        assertFalse(compare.compare("ABC", "АБВ"));
+
+        try {
+            assertFalse(compare.compare("ABC", null));
+            assertFalse(compare.compare(null, "АБВ"));
+
+            fail();
+        } catch (NullPointerException ignored) {} // null.equals("") throws NPE
+    }
+
+    @Test
+    @DisplayName("CompareDeepArrays_Default")
+    void compareDeepArrays_Default() {
+        val compare = generate(CompareDeepArrays.class, EqualityStrategy.CONTENTS);
+
+        assertTrue(compare.compare(
+                new String[][] { new String[] { "1", "2", "3" } },
+                new String[][] { new String[] { "1", "2", "3" } }
+        ));
+
+        assertFalse(compare.compare(
+                new String[][] { new String[] { "1", "2", "3" } },
+                new String[][] { new String[] { "3", "2", "1" } }
+        ));
+    }
+
+    @Test
+    @DisplayName("CompareByteWithInt_Default")
+    void compareByteWithInt_Default() {
+        val compare = generate(CompareByteWithInt.class, EqualityStrategy.DEFAULT);
+        assertTrue(compare.compare((byte) 1, 1));
+        assertFalse(compare.compare((byte) 0, 1));
+    }
+
+    @Test
+    @DisplayName("CompareInts_Default")
+    void compareInts_Default() {
+        val compare = generate(CompareInts.class, EqualityStrategy.DEFAULT);
+        assertTrue(compare.compare(1, 1));
+        assertFalse(compare.compare(0, 1));
+    }
+
+    @SneakyThrows
+    private <T> T generate(final Class<T> compareInterface, final EqualityStrategy strategy) {
+        val type = Javabyte.make(testName);
+        type.setPublicFinal();
+        type.addInterface(compareInterface);
+
+        val method = type.addMethod("compare");
+        method.setPublic();
+        method.copySignatureFrom(compareInterface);
+
+        val code = method.getBytecode();
+        code.loadLocal(1);
+        code.loadLocal(2);
+
+        val position = Asm.position();
+        val endPosition = Asm.position();
+        code.jumpIfNotEquals(strategy, position);
+        code.pushInt(1);
+        code.jump(JumpOpcode.GOTO, endPosition);
+        code.visit(position);
+        code.pushInt(0);
+        code.visit(endPosition);
+        code.callReturn();
+
+        type.writeTo(new File("out"));
+
+        return type.load(TestClassLoader.create())
+                .asSubclass(compareInterface)
+                .newInstance();
+    }
+
+}
---
+++ src/test/java/javabyte/feature/FeatureTests.java
@@ -0,0 +1,113 @@
+/*
+ *    Copyright 2021 Whilein
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package javabyte.feature;
+
+import javabyte.Javabyte;
+import javabyte.TestClassLoader;
+import javabyte.make.MakeClass;
+import javabyte.opcode.FieldOpcode;
+import javabyte.opcode.MethodOpcode;
+import javabyte.type.Types;
+import lombok.SneakyThrows;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.io.File;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+/**
+ * @author whilein
+ */
+final class FeatureTests {
+
+    String testName;
+
+    @BeforeEach
+    void setup(final TestInfo testInfo) {
+        testName = testInfo.getDisplayName();
+    }
+
+    @Test
+    @DisplayName("EqualsAndHashCode")
+    void equalsAndHashCodeTest() {
+        val type = generate(MakeClass::addHashCodeAndEquals);
+        assertNotEquals(null, type);
+    }
+
+    @Test
+    @DisplayName("ToString")
+    void toStringTest() {
+        val type = generate(MakeClass::addToString);
+        assertEquals("ToString[A=true, B=1, C=2, D=A, E=4, F=5, G=6.0, H=7.0, X=Message, Y=[Message A, Message B, Message C], Z=[[Message A, Message B], [Message C, Message D]]]", type.toString());
+    }
+
+    @SneakyThrows
+    private Object generate(final Consumer<MakeClass> initializer) {
+        val type = Javabyte.make(testName);
+        type.setPublicFinal();
+
+        val constructor = type.addConstructor();
+        constructor.setPublic();
+
+        for (int i = Types.BOOL_TYPE; i <= Types.DOUBLE_TYPE; i++) {
+            type.addField(String.valueOf((char) ('A' + i - 1)), Types.getPrimitive(i));
+        }
+
+        type.addField("X", Types.OBJECT);
+        type.addField("Y", Types.OBJECT.dimensions(1));
+        type.addField("Z", Types.OBJECT.dimensions(2));
+
+        val code = constructor.getBytecode();
+        code.loadLocal(0);
+        code.methodInsn(MethodOpcode.SPECIAL, "<init>").inSuper().descriptor(void.class);
+
+        int cnt = 0;
+
+        for (val field : type.getFields()) {
+            constructor.addParameter(field.getType());
+
+            code.loadLocal(0);
+            code.loadLocal(++cnt);
+            code.fieldInsn(FieldOpcode.PUT, field.getName()).inCurrent().descriptor(field.getType());
+        }
+
+        code.callReturn();
+
+        initializer.accept(type);
+
+        type.writeTo(new File("out"));
+
+        return type.load(TestClassLoader.create())
+                .getDeclaredConstructor(boolean.class, byte.class, short.class, char.class, int.class,
+                        long.class, float.class, double.class, Object.class, Object[].class, Object[][].class)
+                .newInstance(
+                        true, (byte) 1, (short) 2, 'A', 4, 5, 6, 7, "Message",
+                        new String[]{"Message A", "Message B", "Message C"},
+                        new String[][]{
+                                new String[]{"Message A", "Message B"},
+                                new String[]{"Message C", "Message D"}
+                        }
+                );
+    }
+
+}
